cc52af22ad137a6c8593166e2c5e1fcdef0693ac
Clay Fudurich <clayfudurich@Justins-Macbook.local>
2017-10-11 13:03:04 -0700

    access keys tokens

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<1SKiccGLvanEphK5QWGOWImGZ>',
+  consumer_secret: '<06ACKgtqK7QNvKFuoODNq999FhoIwVU40TVvhi9iKO3Kv6tJTg>',
+  access_token_key: '<915389366716866561-rirmo69KRT0dOhrHaXinpJpuSRpsu7H>',
+  access_token_secret: '<TbNYxKYFD20syC7sO3xpbQ1IeTDJv50aUoWrC9sn1NE9L>',
+}
