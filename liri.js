dcea51827f30314be8749ef39fe2c211a4e63e88
Clay Fudurich <clayfudurich@Justins-Macbook.local>
2017-10-13 21:21:09 -0700

    finished all functions

--- liri.js
+++ liri.js
@@ -2,6 +2,8 @@ console.log("liri is starting");
 var twitter = require('./twitterCall');
 var spotify = require('./spotifyCall');
 var imdb = require('./IMDBcall');
+var fs = require("fs");
+var inquirer = require('inquirer');
 //------------------------------------------------
 	//grab data from keys.js 
 
@@ -12,28 +14,60 @@ var imdb = require('./IMDBcall');
 	 // more flexibility on user entries - take multiple word song requests
 	 // better feedback on invalid entry 
 
+inquirer.prompt([
+	 {
+	    name: "name",
+	    message: "What can I do for you?",
+	    type: 'list',
+	    choices: ['Read my tweets', 'Spotify a song', 'Look up a movie', 'Other'],
+
+	  }]).then(function(answers) {
+		console.log(answers);
+
+
+
+		    liriFunc = answers.name;
+		    liriFunctions();
+		});
+
 var liriFunc = process.argv[2];
+var param = process.argv[3];
 
-if (liriFunc === 'my-tweets') { 
-	twitter();
+function liriFunctions() {
+	// log user input
+	fs.appendFile("log.txt", liriFunc + ', ' + param + "\n");
 
-} else if (liriFunc === 'spotify-this-song') {
-	if (process.argv[3] == null){
-		spotify("The Sign Ace of Base");
-	} else {
-		spotify(process.argv[3]);
-	};
-	
-}  else if (liriFunc === 'movie-this') {
-	if (process.argv[3] == null){
-		imdb("Mr.Nobody");
-	} else {
-		imdb(process.argv[3]);
-	};
+		if (liriFunc === 'my-tweets' || 'Read my tweets') { 
+			twitter();
 
-}  else if (liriFunc === 'do-what-it-says') {	
+		} else if (liriFunc === 'spotify-this-song' || 'Spotify a song') {
+			if (param == null){
+				spotify("The Sign Ace of Base");
+			} else {
+				spotify(param);
+			};
+			
+		}  else if (liriFunc === 'movie-this' || 'Look up a movie') {
+			if (param == null){
+				imdb("Mr.Nobody");
+			} else {
+				imdb(param);
+			};
 
-} else { 
+		}  else if (liriFunc === 'do-what-it-says' || 'Other') {
+			fs.readFile("random.txt", "utf8" , function (err, data) {
+		    var array = data.toString().split(',');
+		   //  console.log(array);
+		  	// console.log(array[0]);
+		  	liriFunc = array[0];
+		    param = array[1];
+		    liriFunctions();
 
-	console.log("Please enter valid input");
-};
+		});
+
+		} else { 
+
+			console.log("Please enter valid input");
+		};
+}liriFunctions();
+
